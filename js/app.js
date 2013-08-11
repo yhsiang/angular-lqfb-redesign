ab93c31c56dfe82370e86e4ad4d335fcbae44d36
Yuan Hsiang Cheng <lyforever62@hotmail.com>
2013-08-11 18:55:15 +0800

    regen

--- js/app.js
+++ js/app.js
@@ -1,1 +1,137 @@
-function loadLink(e,t){e.location=t,e.links=[{url:"/events",name:"Events"},{url:"/",name:"Issues"},{url:"/units",name:"Units"},{url:"/members",name:"Members"}],e.isActive=function(e){return e==t.path()?"on":""}}function loadAction(e){e.actions=[{name:"new-issue",symbol:"+"}]}function loadProfileMenu(e){e.items=[{name:"※　Profile"}]}function loadIssue(e,t,n,r,i){getIssues=function(){var s=t.list({limit:30});s.$then(function(t){for(var s in t.data)t.data[s].initiatives=n.get({issue_id:t.data[s].id}),t.data[s].area=r.get({area_id:t.data[s].area_id}).$then(function(e){if(e.status=="200")return e.data.unit=i.get({unit_id:e.data.unit_id}),console.log(e.data),e.data});e.issues=t.data})},getIssues(),e.getStateClass=function(e){var t=["discussion","voting","admission","verification"];return t[e%4]},e.getState=function(e){var t=["Discussion","Voting","New","Frozen"];return t[e%4]}}function loadEvent(e){}function loadUnit(e){}function loadMember(e){}angular.module("app.service",["ngResource"]).config(function(e){e.when("/",{controller:loadIssue,templateUrl:"/partials/issues.html"}).when("/events",{controller:loadEvent,templateUrl:"/partials/events.html"}).when("/units",{controller:loadUnit,templateUrl:"/partials/units.html"}).when("/members",{controller:loadMember,templateUrl:"/partials/members.html"}).otherwise({redirect:"/"})}).factory("Issues",function(e,t){return e("http://apitest.liquidfeedback.org\\:25520/issue",{callback:"JSON_CALLBACK"},{get:{method:"get",isArray:!1},list:{method:"jsonp",params:{limit:"@limit"},isArray:!0,transformResponse:function(e,t){return e.result}}})}).factory("Initiative",function(e){return e("http://apitest.liquidfeedback.org\\:25520/initiative",{alt:"json",callback:"JSON_CALLBACK"},{get:{method:"jsonp",params:{issue_id:"@issue_id"},isArray:!0,transformResponse:function(e,t){return e.result}},list:{method:"jsonp",isArray:!0,transformResponse:function(e,t){return e.result}}})}).factory("Area",function(e){return e("http://apitest.liquidfeedback.org\\:25520/area",{alt:"json",callback:"JSON_CALLBACK"},{get:{method:"jsonp",params:{area_id:"@area_id"},isArray:!1,transformResponse:function(e,t){return e.result[0]}},list:{isArray:!0,method:"jsonp",transformResponse:function(e,t){return e.result}}})}).factory("Unit",function(e){return e("http://apitest.liquidfeedback.org\\:25520/unit",{alt:"json",callback:"JSON_CALLBACK"},{get:{method:"jsonp",params:{unit_id:"@unit_id"},isArray:!1,transformResponse:function(e,t){return e.result[0]}},list:{isArray:!0,method:"jsonp",transformResponse:function(e,t){return e.result}}})}).factory("Member",function(e){return e("http://apitest.liquidfeedback.org\\:25520/member",{alt:"json",callback:"JSON_CALLBACK"},{get:{method:"get",isArray:!1},list:{isArray:!0,method:"jsonp",transformResponse:function(e,t){return e.result}}})}),angular.module("app",["app.service"])
+angular.module('app.service',['ngResource'])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {controller:loadIssue, templateUrl:'/partials/issues.html'})
+            .when('/events', {controller:loadEvent, templateUrl:'/partials/events.html'})
+            .when('/units', {controller:loadUnit, templateUrl:'/partials/units.html'})
+            .when('/members', {controller:loadMember, templateUrl:'/partials/members.html'})
+            .otherwise({redirect:'/'});
+    })
+    .factory('Issues', function ($resource, Initiative){
+        return $resource('http://apitest.liquidfeedback.org\\:25520/issue',
+            {callback:'JSON_CALLBACK'},{
+            get: {method: 'get', isArray:false},
+            list:{method:'jsonp', params:{limit: '@limit'}, isArray:true,
+                  transformResponse: function (data, headers) {
+                    return data.result;
+                }}
+        });
+    }) // factoring 'Issues' to handle $resource response 
+    .factory('Initiative', function ($resource){
+        return $resource('http://apitest.liquidfeedback.org\\:25520/initiative',
+            {alt:'json', callback:'JSON_CALLBACK'},{
+            get: {method: 'jsonp', params:{issue_id: '@issue_id'}, isArray:true,
+                  transformResponse: function (data, headers) {
+                    return data.result;
+                }},
+            list:{method:'jsonp', isArray:true,
+                  transformResponse: function (data, headers) {
+                    return data.result;
+                }}
+        });        
+    })
+    .factory('Area', function ($resource){
+        return $resource('http://apitest.liquidfeedback.org\\:25520/area', 
+                    {alt:'json', callback:'JSON_CALLBACK'},{
+            get: {method: 'jsonp', params:{area_id: '@area_id'}, isArray:false,
+                  transformResponse: function (data, headers) {
+                    return data.result[0];
+                }},
+            list:{isArray:true, method:'jsonp',
+                  transformResponse: function (data, headers) {
+                    return data.result;
+                }}   
+        });    
+    })
+    .factory('Unit', function ($resource){
+        return $resource('http://apitest.liquidfeedback.org\\:25520/unit', 
+                    {alt:'json', callback:'JSON_CALLBACK'},{
+            get: {method: 'jsonp', params:{unit_id: '@unit_id'}, isArray:false,
+                  transformResponse: function (data, headers) {
+                    return data.result[0];
+                }},            
+            list:{isArray:true, method:'jsonp',
+                  transformResponse: function (data, headers) {
+                    return data.result;
+                }}   
+        });    
+    })
+    .factory('Member', function ($resource){
+        return $resource('http://apitest.liquidfeedback.org\\:25520/member', 
+                    {alt:'json', callback:'JSON_CALLBACK'},{
+            get: {method: 'get', isArray:false},
+            list:{isArray:true, method:'jsonp',
+                  transformResponse: function (data, headers) {
+                    return data.result;
+                }}   
+        });    
+    }); 
+angular.module('app',['app.service']);
+
+function loadLink($scope, $location) {
+    $scope.location = $location;
+    $scope.links = [
+         { url: "/events", name: "Events"},
+         { url: "/", name: "Issues"},
+         { url: "/units", name: "Units"},
+         { url: "/members", name: "Members"}
+    ];
+    $scope.isActive = function (url) {
+        return (url == $location.path()) ? 'on' : '';
+    }
+}
+
+function loadAction($scope) {
+    $scope.actions = [
+        { name:"new-issue", symbol: "+"}
+    ];
+}
+
+function loadProfileMenu($scope) {
+
+    $scope.items = [
+        {name: "※　Profile"}
+    ];
+}
+function loadIssue($scope, Issues, Initiative, Area, Unit) { //list all the issue in json file
+
+    getIssues = function () {
+        var promise = Issues.list({limit: 30});
+        promise.$then(function(result){
+            for(var k in result.data) {
+                result.data[k].initiatives = Initiative.get({issue_id: result.data[k].id});
+                result.data[k].area = Area
+                    .get({area_id: result.data[k].area_id})
+                    .$then(function(value) {
+                        if(value.status == '200') {
+                            value.data.unit = Unit.get({unit_id: value.data.unit_id});
+                            console.log(value.data);
+                            return value.data;
+                        }
+                    });
+            }
+            $scope.issues = result.data;
+        });
+    }
+    getIssues();
+    $scope.getStateClass = function (index) {
+        var css = ["discussion", "voting", "admission", "verification"];
+        return css[index%4];
+    }
+    $scope.getState = function (index) {
+        var css = ["Discussion", "Voting", "New", "Frozen"];
+        return css[index%4];
+    }
+}
+
+function loadEvent($scope) {
+
+}
+
+function loadUnit($scope) {
+
+}
+
+function loadMember($scope) {
+
+}
